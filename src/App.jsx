9a242e4b7a8ed3a0b91e023a3b68324fd6809297
SyedAteef1 <vsyedateefquadri@gmail.com>
2025-06-09 14:21:09 +0530

    the error

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import { Navbar } from './Components/navbar'
 import HomePage from './Components/HomePage'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Resume from './Components/resume'
 function App() {
-  const [count, setCount] = useState(0)
-  const [name,setName]=useState("")
+
     return (
       <BrowserRouter>
       <Navbar></Navbar>
